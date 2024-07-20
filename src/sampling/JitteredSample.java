f5014a791fe846e5a612941e0c838fbafa1a8421
Chrissblau08 <66316035+Chrissblau08@users.noreply.github.com>
2024-07-20 21:59:46 +0200

    Uebung 3

--- src/sampling/JitteredSample.java
+++ src/sampling/JitteredSample.java
@@ -1,13 +1,23 @@
 package sampling;
 
-import projection.Projection;
+import main.Driver;
 import utility.Point2D;
-import utility.Ray;
+import java.util.Random;
 
 public class JitteredSample extends Sampler
 {
+    private Random random;
+    public JitteredSample(int samples)
+    {
+        this.samples = samples;
+        random = new Random();
+    }
+
     @Override
     public Point2D sample(int row, int col, int x, int y) {
-        return null;
+        Point2D point = new Point2D();
+        point.x = x - Driver.world.viewPlane.width/2+(col+random.nextFloat())/samples;
+        point.y = y - Driver.world.viewPlane.height/2+(row+random.nextFloat())/samples;
+        return point;
     }
 }
